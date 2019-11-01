7fd6e56db797944b3621c3d9f99d8efe4ddbca17
Beer van der Drift <beerdrift@gmail.com>
2019-11-01 15:07:37 +0100

    chore: make parameter types more specific

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,34 @@
-import { EventHandler, SyntheticEvent } from 'react';
-
 /**
  * A custom React Hook that provides a declarative useEventListener.
  */
-declare function useEventListener<T extends SyntheticEvent<any>>(
+declare function useEventListener<K extends keyof HTMLElementEventMap>(
+  eventName: K,
+  handler: HTMLElementEventMap[K],
+  element: HTMLElement
+);
+declare function useEventListener<K extends keyof DocumentEventMap>(
+  eventName: K,
+  handler: DocumentEventMap[K],
+  element: Document
+);
+declare function useEventListener<K extends keyof WindowEventMap>(
+  eventName: K,
+  handler: WindowEventMap[K],
+  element?: Window
+);
+declare function useEventListener(
   eventName: string,
-  handler: EventHandler<T>,
-  element?: HTMLElement
+  handler: EventListenerOrEventListenerObject,
+  element?: HTMLElement | Window | Document
+);
+declare function useEventListener<
+  K extends keyof (HTMLElementEventMap & DocumentEventMap & WindowEventMap)
+>(
+  eventName: K,
+  handler: (
+    event: (HTMLElementEventMap & DocumentEventMap & WindowEventMap)[K]
+  ) => void,
+  element?: HTMLElement | Document | Window
 ): void;
 
 export as namespace useEventListener;
