58879da5157e06f87040432847aec8c3c5df7472
donavon <github@donavon.com>
2020-07-26 07:44:20 -0400

    update to @testing-library/jest-dom

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks';
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 
 import useEventListener from '../src';
 
